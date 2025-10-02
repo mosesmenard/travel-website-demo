e51a461e9223aafa1acaee1afcb7074aa993d946
mosesmenard <menardowuor@gmail.com>
2025-10-02 18:22:57 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./*.{html,php}",
+    "./pages/**/*.{html,php,js}",
+    "./components/**/*.{html,php,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
